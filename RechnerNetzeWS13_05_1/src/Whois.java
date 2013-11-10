8a8fae1bf29b13e93f90726fc2f5c40e552d442f
Raoul <Raoul@Raoul-PC.home>
2013-11-10 16:48:50 +0100

    kommentare

--- RechnerNetzeWS13_05_1/src/Whois.java
+++ RechnerNetzeWS13_05_1/src/Whois.java
@@ -9,30 +9,34 @@ public class Whois {
 	
 	public static void main(String[] args) {
 		
-		String serverName = System.getProperty("WHOIS_SERVER", DEFAULT_HOST);  
+		String serverName = System.getProperty("WHOIS_SERVER", DEFAULT_HOST);  // 
 	    
-	    InetAddress server = null;
+	    InetAddress server = null; //initialisieren des Servers als Inet Adresse (klasse die Jave zur Verfügung stellt und eine Internetadresse repräsentiert)
 	    try {
-	      server = InetAddress.getByName(serverName);
+	      server = InetAddress.getByName(serverName);// Klassenmethode von InetAdress die die IP Adresse vom servername holt
 	    }
 	    catch (UnknownHostException ex) {
-	      System.err.println("Error: Could not locate whois server "  
-	       + server);  //Whois Server Abfrage kann nicht durchgeführt werden
+	      System.err.println("Error: Could not locate whois server " + server);  //Whois Server Abfrage kann nicht durchgeführt werden
 	      System.err.println("Usage: java -DWHOIS_SERVER=hostname WhoisClient name");   //CMD Kommandozeilenparameter       
 	      return;
 	    }       
 
 	    try {
-	      Socket theSocket = new Socket(server, DEFAULT_PORT);
-	      Writer out = new OutputStreamWriter(theSocket.getOutputStream(), 
-	       "8859_1");
-	      for (int i = 0; i < args.length; i++) out.write(args[i] + " ");
+	      Socket theSocket = new Socket(server, DEFAULT_PORT); //Socket baut die Verbindung auf
+	      Writer out = new OutputStreamWriter(theSocket.getOutputStream(), "8859_1"); //iso Standard Latin 1 
+	      for (int i = 0; i < args.length; i++) 
+	      {
+	    	  out.write(args[i] + " ");
+	      }
 	      out.write("\r\n");
-	      out.flush();
-	      InputStream raw = theSocket.getInputStream();
-	      InputStream in  = new BufferedInputStream(theSocket.getInputStream());
-	      int c;
-	      while ((c = in.read()) != -1) System.out.write(c);
+	      out.flush(); //jetzt wird aau.at an den Server gesendet
+	      //InputStream raw = theSocket.getInputStream();
+	      InputStream in  = new BufferedInputStream(theSocket.getInputStream());//lesen was Server zurückgibt
+	      int c; // int = 4 Byte daher wird immer 1 Byte gelesen
+	      while ((c = in.read()) != -1) //wenn -1 gelesen wird, dann Ende (von BufferedInputStream so definiert)
+	      {
+	    	  System.out.write(c); //Byte für Byte wird ausgegeben und von Java in Text umgewandelt
+	      }
 	    }
 	    catch (IOException ex) {
 	      System.err.println(ex);
